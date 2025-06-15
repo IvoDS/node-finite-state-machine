5d273b67bc95fd55b4182d120d7903e29dd67102
IvoDS <43913533+IvoDS@users.noreply.github.com>
2025-06-15 22:57:07 +0200

    Aggiunto Readme e controlli di primo passaggio in stato e sottostato.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # node-finite-state-machine
 A Node project to handle Finite State Machines in a Node/TypeScript environment.
+
+## Why node-finite-state-machine in an event-driven framework?
+This package was created by a Firmware Developer as a test, to try and implement microcontroller programming concepts in a system born to work in a completely different manner.
+The idea is to implement a loop which will execute a Finite State Machine, just like the main() function of a microcontroller does.
+
+## How To Start
+The NPM package will be deployed as a .tgz archive to be downloaded from this repository; this will need to be saved to your PC in a known folder.  
+Once saved, add its path in the _dependencies_ section of your _package.json_ file.
+
+> "dependencies": {  
+    "node-finite-state-machine": "file:path-to-tgz-archive"  
+  }
+
+Once you have done this, cd to the root directory of your project and
+
+```bash
+npm install
+```
+
+That is all!
--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,17 @@
  */
 
 /**
- * Typing the Finite State Machine object to
- * conform it to a specific structure.
+ * Enumeration for timer index
  */
-type FSM_WORK = {
-    
+enum FSM_TIMER {
+    TIMER_1 = 0,
+    TIMER_2 = 1,
+    TIMER_3 = 2,
+    TIMER_4 = 3
 };
 
 /**
- * Finite State Machine class to create an object
- * of the aforementioned FSM_WORK type;
+ * Finite State Machine class.
  */
 class FiniteStateMachine {
     private curr_state:number;
@@ -24,11 +25,10 @@ class FiniteStateMachine {
     private curr_substate:number;
     private prev_substate:number;
     private max_substate:number;
-    private timer1:number;
-    private timer2:number;
-    private timer3:number;
-    private timer4:number;
-    private name:string;    
+    private timers: { startTime: number; duration: number }[];
+    private name:string;
+    private first_in_state:boolean;
+    private first_in_substate:boolean;
 
     /**
      * Constructor where all the initial values are required
@@ -49,11 +49,95 @@ class FiniteStateMachine {
         this.max_state = max_state;
         this.curr_substate = this.prev_substate = start_substate;
         this.max_substate = max_substate;
-        this.timer1 = this.timer2 = this.timer3 = this.timer4 = 0;
+        this.timers = Array(4).fill(null).map(() => ({ startTime: 0, duration: 0 }));
+        this.first_in_state = true;
+        this.first_in_substate = true;
     }
+
+    /**
+     * Functions to get and set the state
+     * @param new_state New value for the state
+     */
+    public set state(new_state:number) {
+        if(new_state <= this.max_state)
+        {
+            this.prev_state = this.curr_state;
+            this.curr_state = new_state;
+        }        
+    }
+
+    public get state() {
+        return this.curr_state;
+    }
+
+    /**
+     * Functions to get and set the substate
+     * @param new_substate New value for the substate
+     */
+    public set substate(new_substate:number) {
+        if(new_substate <= this.max_substate) {
+            this.prev_substate = this.curr_substate;
+            this.curr_substate = new_substate;
+        }
+    }
+
+    public get substate() {
+        return this.curr_substate;
+    }
+
+    /**
+     * Function to start a timer in milliseconds.
+     * @param index      From FSM_TIMER enum
+     * @param durationMs Duration of the interval in milliseconds
+     */
+    fsm_set_timer(index: FSM_TIMER, durationMs: number): void {
+    if (index < FSM_TIMER.TIMER_1 || index > FSM_TIMER.TIMER_3) {
+      throw new Error(`[${this.name}]Indice del timer non valido (deve essere 1, 2, 3 o 4)`);
+    }
+    this.timers[index] = {
+      startTime: Date.now(),
+      duration: durationMs
+    };
+  }
+
+  /**
+   * Checks if the specified amount of milliseconds has elapsed.
+   * @param index Index of the timer to check
+   * @returns true if the specified ms have elapsed
+   */
+  fsm_check_timer(index: number): boolean {
+    if (index < FSM_TIMER.TIMER_1 || index > FSM_TIMER.TIMER_3) {
+      throw new Error(`[${this.name}]Indice del timer non valido (deve essere 1, 2, 3 o 4)`);
+    }
+    const timer = this.timers[index - 1];
+    return Date.now() - timer!.startTime >= timer!.duration;
+  }
+
+  /**
+   * Checks if it's the first time that the FSM passes in the state
+   */
+  public is_fsm_first_in_state() {
+    let retval:boolean = this.first_in_state;
+
+    if(this.first_in_state)
+        this.first_in_state = false;
+
+    return retval;
+  }/**
+   * Checks if it's the first time that the FSM passes in the state
+   */
+  public is_fsm_first_in_substate() {
+    let retval:boolean = this.first_in_substate;
+
+    if(this.first_in_substate)
+        this.first_in_substate = false;
+
+    return retval;
+  }
+
 }
 
-export type {
-    FSM_WORK,
+export {
+    FSM_TIMER,
     FiniteStateMachine
 }
